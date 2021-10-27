7d319f3911fb943f6acaf29c6496de4ca6b16e9b
Дмитрий Ткачук <tkachuk@experium.ru>
2021-10-27 20:15:45 +0300

    Fix error path

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -2,5 +2,5 @@ import { pathEq } from 'ramda';
 
 export const isError = (error, serverErrors) => (
     !!pathEq(['graphQLErrors', 0, 'message'], error, serverErrors)
-    || !!pathEq(['message'], error, serverErrors)
+    || !!pathEq(['data', 'message'], error, serverErrors)
 );
