6880fb3edc00a65d390b423ba6756a2dbe713a09
Дмитрий Ткачук <tkachuk@experium.ru>
2021-10-27 23:15:20 +0300

    Fix form widget request error handle

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -3,4 +3,5 @@ import { pathEq } from 'ramda';
 export const isError = (error, serverErrors) => (
     !!pathEq(['graphQLErrors', 0, 'message'], error, serverErrors)
     || !!pathEq(['data', 'message'], error, serverErrors)
+    || !!pathEq(['message'], error, serverErrors)
 );
